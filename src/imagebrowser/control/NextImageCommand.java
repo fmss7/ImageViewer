5b841cf93144c420f25e76dffd0af7d1d85b4b64
Francisco <Francisco@Francisco-PC>
2015-01-05 22:51:28 +0000

    05/01/14 || 22:50

---
+++ src/imagebrowser/control/NextImageCommand.java
@@ -0,0 +1,17 @@
+package imagebrowser.control;
+
+import imagebrowser.ui.ImageViewer;
+
+public class NextImageCommand extends ImageCommand {
+
+    public NextImageCommand(ImageViewer viewer) {
+        super(viewer);
+    }
+    
+    @Override
+    public void execute() {
+        this.getViewer().showNextImage();
+        
+    }
+
+}
